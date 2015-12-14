86f26067a36aed1a9fca405f6f95684acf7d11fc
NHerbert141 <NHerbert@Ron.local>
2015-12-13 21:58:57 -0500

    added website files

---
+++ Tidal_Peat/Photos_files/Photos.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.3 local-build-20151213
+
+function createMediaStream_id2()
+{return IWCreatePhotocast("http://www.tidalpeat.com/Tidal_Peat/Photos_files/rss.xml",true);}
+function initializeMediaStream_id2()
+{createMediaStream_id2().load('http://www.tidalpeat.com/Tidal_Peat',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id2',{pageIndex:0}));});}
+function layoutMediaGrid_id2(range)
+{createMediaStream_id2().load('http://www.tidalpeat.com/Tidal_Peat',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(3,new IWSize(165,165),new IWSize(165,0),new IWSize(204,180),27,27,0,new IWSize(2,2)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:1,transitionIndex:9},'Media/slideshow.html','widget1','widget2','widget3')});}
+function relayoutMediaGrid_id2(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id2(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id2(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Photos_files/PhotosMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');NotificationCenter.addObserver(null,relayoutMediaGrid_id2,'RangeChanged','id2')
+adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');Widget.onload();fixAllIEPNGs('Media/transparent.gif');initializeMediaStream_id2()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
